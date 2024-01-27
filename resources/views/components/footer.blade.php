bf16ed353544192c7002d031541d888c0771619b
dmigwi <migwindungu0@gmail.com>
2024-01-27 18:44:43 +0100

    Add the attendance page UI

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="space-x-4 text-center bg-gold pt-6">
+<div class="space-x-4 text-center bg-light-blue pt-6">
     <span>Class Management System</span>
     <span class="text-sm hover:text-blue-200">&copy;2024</span>
     <a href="/contacts" class="text-sm hover:">Contact</a>
