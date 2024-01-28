0846f8209120ba8e0b4c12b176271052bf974e3e
dmigwi <migwindungu0@gmail.com>
2024-01-28 18:41:37 +0100

    Add sign out svg icon
    use group icon to show attendance

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -23,7 +23,10 @@
         ring-opacity-5 focus:outline-none h-fit" role="menu" aria-orientation="vertical" aria-labelledby="hs-dropdown-btn" tabindex="-1">
           <button class="text-gray-700 block px-4 py-0 text-sm w-full" type="button" role="menuitem" tabindex="-1" id="menu-item-0">
             <div class="flex sm:items-center justify-between py-2 w-fit">
-                <span class="relative flex items-center space-x-4"> Sign out </span>
+              <span class="relative flex items-center space-x-4"> Sign out </span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" class="mx-2" fill="currentColor" viewBox="0 0 520 520">
+                <path d="M210 485v-30c0-8-7-15-15-15H95c-8 0-15-7-15-15V95c0-8 7-15 15-15h100c8 0 15-7 15-15V35c0-8-7-15-15-15H60a40 40 0 0 0-40 40v400a40 40 0 0 0 40 40h135c8 0 15-7 15-15zm286-215c6-6 6-15 0-21L361 114c-6-6-15-6-21 0l-21 21c-6 6-6 15 0 21l56 56c6 6 2 17-7 17H155c-8 0-15 6-15 14v30c0 8 7 16 15 16h212c9 0 13 11 7 17l-56 56c-6 6-6 15 0 21l21 21c6 6 15 6 21 0l136-134z"/>
+              </svg>
             </div>
           </button>
         </div>
