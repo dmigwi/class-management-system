9c9d3ffdebb7717580709ebbac27330ce92eb5e6
dmigwi <migwindungu0@gmail.com>
2024-01-28 11:45:42 +0100

    Add class select button in chat

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -10,13 +10,6 @@
     <img src="images/cms.png" loading="lazy" class="ml-4 w-40" alt="Class Management System logo" />
     <div class="relative z-20 flex flex-col justify-center h-full px-3 md:w-full">
         <div class="relative flex items-center justify-end w-full space-x-4">
-            <button class="flex items-center p-2 text-gray-700 bg-white rounded-full shadow hover:text-gray-700 text-md">
-                <svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M912 1696q0-16-16-16-59 0-101.5-42.5t-42.5-101.5q0-16-16-16t-16 16q0 73 51.5 124.5t124.5 51.5q16 0 16-16zm816-288q0 52-38 90t-90 38h-448q0 106-75 181t-181 75-181-75-75-181h-448q-52 0-90-38t-38-90q50-42 91-88t85-119.5 74.5-158.5 50-206 19.5-260q0-152 117-282.5t307-158.5q-8-19-8-39 0-40 28-68t68-28 68 28 28 68q0 20-8 39 190 28 307 158.5t117 282.5q0 139 19.5 260t50 206 74.5 158.5 85 119.5 91 88z">
-                    </path>
-                </svg>
-            </button>
-            <span class="w-1 h-8 bg-gray-300 rounded-lg"></span>
             <button class="flex items-center text-black-500 dark:text-white text-md">
                 <span class="pr-2"> Charlie Rector</span>
                 <span class="mx-auto object-cover rounded-full h-10 w-10 student-account pl-2"></span>
