276be5ada2fb8deee2c5602dbc9ca7c2d01b8ac8
dmigwi <migwindungu0@gmail.com>
2024-02-27 04:49:06 +0100

    Update the chats seed data

--- database/migrations/2024_01_30_084822_create_chats_table.php
+++ database/migrations/2024_01_30_084822_create_chats_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
             $table->id();
             $table->text('message');
             $table->unsignedBigInteger('unit_id');
-            $table->unsignedBigInteger('user_id')->nullable();;
+            $table->unsignedBigInteger('sender')->nullable();
             $table->timestamp('read_at')->useCurrentOnUpdate()->nullable();
             $table->timestamp('created_at')->useCurrent();
             $table->foreign('unit_id')->references('id')->on('units')->onDelete('cascade');
-            $table->foreign('user_id')->nullable()->constrained()->references('id')->on('users')->onDelete('set null');
+            $table->foreign('sender')->nullable()->constrained()->references('id')->on('users')->onDelete('set null');
         });
     }
 
